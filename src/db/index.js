2af0b9d9b90f3be6e9f65e70e75eee2d53075e31
Nazhat Adiba <m.nazhat.adiba@gmail.com>
2024-03-20 22:26:13 +0700

    added env

--- src/db/index.js
+++ src/db/index.js
@@ -1,33 +1,43 @@
 // Import the functions you need from the SDKs you need
-import firebase from 'firebase/compat/app'
-import 'firebase/compat/firestore'
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
 
 import { initializeApp } from "firebase/app";
-import { getFirestore } from 'firebase/firestore'
+import { getFirestore } from "firebase/firestore";
 
+const {
+  VITE_FIREBASE_API_KEY,
+  VITE_FIREBASE_AUTH_DOMAIN,
+  VITE_FIREBASE_PROJECT_ID,
+  VITE_FIREBASE_STORAGE_BUCKET,
+  VITE_FIREBASE_MESSAGING_SENDER_ID,
+  VITE_FIREBASE_APP_ID,
+  VITE_FIREBASE_MEASUREMENT_ID,
+} = import.meta.env;
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBLYqH2XKv4-cDk0JNKzx3NcuD8Aez8ztk",
-  authDomain: "servicario-2.firebaseapp.com",
-  projectId: "servicario-2",
-  storageBucket: "servicario-2.appspot.com",
-  messagingSenderId: "689605931097",
-  appId: "1:689605931097:web:77a9dc14ae7b8ff5c24148",
-  measurementId: "G-L141CTVG3P"
+  apiKey: VITE_FIREBASE_API_KEY,
+  authDomain: VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: VITE_FIREBASE_PROJECT_ID,
+  storageBucket: VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: VITE_FIREBASE_APP_ID,
+  measurementId: VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 
 //namedspace SDK
-export const nDb = firebase.initializeApp(firebaseConfig).firestore()
+export const nDb = firebase.initializeApp(firebaseConfig).firestore();
 
 //modular SDK
 const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
-export const db = getFirestore(app)
+export const db = getFirestore(app);
 
 // export {Timestamp}

db5befc737d436a9c93b90a9b7d4def26b948b40
Nazhat Adiba <m.nazhat.adiba@gmail.com>
2024-03-21 09:36:44 +0700

    added handlechange props

--- src/components/input/BasicInput.jsx
+++ src/components/input/BasicInput.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 
-const BasicInput = ({ label, placeholder, type = "input" }) => {
+const BasicInput = ({
+  label,
+  placeholder,
+  type = "input",
+  handleChange,
+  id,
+}) => {
+  const inputProps = handleChange;
   return (
     <div className="flex flex-col">
-      <label htmlFor="input_1" className="mb-2">
+      <label htmlFor={id} className="mb-2">
         {label}
       </label>
       <input
-        id="input_1"
+        onChange={inputProps.onChange}
+        value={inputProps.data}
+        id={id}
         type={type}
         className="px-4 py-2 bg-white border-2 border-gray-100"
         placeholder={placeholder}

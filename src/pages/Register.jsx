3472027c3546830ab4dfa0891944105366d3031d
Nazhat Adiba <m.nazhat.adiba@gmail.com>
2024-03-21 09:37:32 +0700

    added register function and hook

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 import BasicInput from "../components/input/BasicInput";
+import useInput from "../hooks/useInput";
+import { nApp } from "../db";
 
 function Register() {
+  const emailHook = useInput();
+  const passwordHook = useInput();
+
+  const registerUser = async (e) => {
+    e.preventDefault(); // Prevent default form submission behavior
+    try {
+      const userCredential = await nApp
+        .auth()
+        .createUserWithEmailAndPassword(emailHook.data, passwordHook.data);
+      const user = userCredential.user;
+      console.log("success", user);
+    } catch (error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.error("Error registering user:", errorCode, errorMessage);
+    }
+  };
+
   return (
     <div className="container mx-auto w-full h-screen flex justify-center items-center bg-gray-50">
       <div className="px-6 py-10 w-full max-w-lg bg-white shadow-lg">
@@ -9,17 +29,19 @@ function Register() {
           <h1 className="text-4xl">Register</h1>
           <p>Please register to proceed</p>
         </header>
-        <form className="flex flex-col gap-4">
+        <form onSubmit={registerUser} className="flex flex-col gap-4">
           <BasicInput
+            id="email"
+            handleChange={emailHook}
             type="email"
             label="Email"
             placeholder="something@email.com"
           />
-          <BasicInput label="Full Name" placeholder="John Doe" />
-          <BasicInput type="password" label="Password" placeholder="Password" />
           <BasicInput
+            id="password"
+            handleChange={passwordHook}
             type="password"
-            label="Repeat Password"
+            label="Password"
             placeholder="Password"
           />
           <button
